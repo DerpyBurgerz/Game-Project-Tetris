09479b5dff2d445b7989b273a724ae9082a86225
DerpyBurgerz <samuel@timetoknow.nl>
2023-10-09 12:19:49 +0200

    tetrominoes toegevoegd

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Game-Project-Tetris
 Sven Wijnen-9091688
 Samuel Eikelenboom-2611872
+
+https://tetris.wiki/Tetris_Guideline
+https://tetris.wiki/Super_Rotation_System
---
+++ Tetris/TetrisTemplate/Tetrominos/O.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisTemplate.Blocks
+{
+	internal class I : Tetrominos
+	{
+		bool[,] Tetromino;
+		I()
+		{
+			bool[,] Tetromino = 
+			{
+				{false, true, false, false },
+				{false, true, false, false },
+				{false, true, false, false },
+				{false, true, false, false },
+			};
+		}
+	}
+}
